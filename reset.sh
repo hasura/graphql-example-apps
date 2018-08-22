25ed9970c98c8ca41101359dee9e1597742499a2
Aravind Shankar <aravind@hasura.io>
2018-08-22 17:36:59 +0530

    add cloudflare scripts

--- reset.sh
+++ reset.sh
@@ -5,5 +5,5 @@ set -evo pipefail
 for D in `find * -maxdepth 0 -type d`
 do
   echo "Resetting app ${D}"
-  ( cd $D && heroku pg:reset --app $D --confirm $D && heroku restart --app $D && hasura migrate apply && hasura metadata apply )
+  ( cd $D && heroku pg:reset --app $D --confirm $D && heroku restart --app $D && hasura migrate apply && hasura metadata apply && sed -e 's/:[^:\/\/]/="/g;s/$/"/g;s/ *=/=/g' config.yaml > ../domain.sh && source ../domain.sh && heroku domains:add ${subdomain}.hasura.app --app $D || true && cname=$(heroku domains --app ${D} --json | jq --arg v "$subdomain.hasura.app" -c '.[] | select( .hostname | contains($v))' | jq -r '.cname') && cli4 --post type="CNAME" proxied=true name="${subdomain}" content="${cname}" /zones/:hasura.app/dns_records || true && cli4 --patch proxied=true type="CNAME" name="${subdomain}" content="${cname}" /zones/:hasura.app/dns_records/:${subdomain}.hasura.app)
 done
